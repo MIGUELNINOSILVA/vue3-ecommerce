0f588ea7d8e944e49743eca5cb1ddf5851cef742
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-11-23 12:30:30 -0500

    Add basic examples with composition api

---
+++ src/components/Contador.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Contador</h1>
+  <h2>{{ count }}</h2>
+  <button @click="count = count+1">Click me</button>
+</template>
+
+<script lang="ts" setup>
+import { ref, type Ref } from 'vue';
+let count:Ref<number>  =  ref(0);
+
+</script>
+
+<style>
+
+</style>
